0c74b577e70d115451a2cd96904936d0050f2172
umairahmed88 <umairahmed_88@ymail.com>
2023-10-07 15:45:49 +0500

    created pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import SignUp from "./pages/SignUp";
+
 export default function App() {
-	return <div className=' text-green-500'>App</div>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/' element={<Home />} />
+				<Route path='/sign-in' element={<SignIn />} />
+				<Route path='/sign-up' element={<SignUp />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/profile' element={<Profile />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
