a6433a28f90a63e7f57d16f6382bc6112b5efafe
umairahmed88 <umairahmed_88@ymail.com>
2023-10-09 13:04:45 +0500

    added sign out functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -14,6 +14,9 @@ import {
 	deleteUserFailure,
 	deleteUserStart,
 	deleteUserSuccess,
+	signOutUserStart,
+	signOutUserFailure,
+	signOutUserSuccess,
 } from "../redux/user/userSlice";
 
 export default function Profile() {
@@ -128,6 +131,22 @@ service firebase.storage {
 		}
 	};
 
+	const handleSignOut = async () => {
+		try {
+			dispatch(signOutUserStart());
+			const res = await fetch("/api/auth/signout");
+			const data = res.json();
+
+			if (data.success === false) {
+				dispatch(signOutUserFailure(data.message));
+				return;
+			}
+			dispatch(signOutUserSuccess());
+		} catch (error) {
+			dispatch(signOutUserFailure(error.message));
+		}
+	};
+
 	return (
 		<div className=' p-3 max-w-lg mx-auto'>
 			<h1 className=' text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -197,7 +216,9 @@ service firebase.storage {
 				>
 					Delete Account
 				</span>
-				<span className=' text-red-700 cursor-pointer'>Sign Out</span>
+				<span onClick={handleSignOut} className=' text-red-700 cursor-pointer'>
+					Sign Out
+				</span>
 			</div>
 			<p className=' text-red-700 mt-5'>{error ? error : ""}</p>
 			<p className=' text-green-700 mt-5'>
