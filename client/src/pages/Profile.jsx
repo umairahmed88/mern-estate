6037caf520e51e1668d771a45c15fcfde8b8b796
umairahmed88 <umairahmed_88@ymail.com>
2023-10-10 13:18:37 +0500

    completed create listing page UI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -18,6 +18,7 @@ import {
 	signOutUserFailure,
 	signOutUserSuccess,
 } from "../redux/user/userSlice";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
 	const fileRef = useRef(null);
@@ -208,6 +209,12 @@ service firebase.storage {
 				>
 					{loading ? "Loading..." : "Update"}
 				</button>
+				<Link
+					className=' bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'
+					to='/create-listing'
+				>
+					Create Listing
+				</Link>
 			</form>
 			<div className='flex justify-between mt-5'>
 				<span
